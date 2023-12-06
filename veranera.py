6e923ca6bfa8a9fc287528ee12d8a7cb5c5d81ed
wifitanmalo <nicolas.chaparro@correounivalle.edu.co>
2023-12-05 19:10:02 -0500

    Plates table added

--- veranera.py
+++ veranera.py
@@ -7,16 +7,16 @@ def home():
     global text, home_frame
     home_frame = tk.Frame(window)
     home_frame.pack()
-    welcome = tk.Label(home_frame, text = "Welcome to",
+    welcome = tk.Label(home_frame, text="Welcome to",
                        font = ("Verdana", 18))
     welcome.pack()
-    veranera = ttk.Label(home_frame, image = logo)
+    veranera = ttk.Label(home_frame, image=logo)
     veranera.pack()
-    sign_in = tk.Button(home_frame, text = "Sign in", command = signin)
+    sign_in = tk.Button(home_frame, text="Sign in", command=signin)
     sign_in.pack()
-    sign_up = tk.Button(home_frame, text = "Sign up", command = signup)
+    sign_up = tk.Button(home_frame, text="Sign up", command=signup)
     sign_up.pack()
-    text = tk.Label(home_frame, text = "", font = ("Verdana", 10))
+    text = tk.Label(home_frame, text="", font=("Verdana", 10))
     text.pack()
 
 # Function to enter the email
@@ -25,41 +25,38 @@ def enter_email():
     home_frame.pack_forget() # Deletes the home elements
     account_frame = tk.Frame(window)
     account_frame.pack()
-    email_text = tk.Label(account_frame, text = "Email:")
+    email_text = tk.Label(account_frame, text="Email:")
     email_text.pack()
-    the_email = tk.Entry(account_frame, width = 22)
+    the_email = tk.Entry(account_frame, width=22)
     the_email.pack()
-    email_error = tk.Label(account_frame, text = "")
+    email_error = tk.Label(account_frame, text="")
     email_error.pack()
 
 # Function to delete the text of the email
 def empty_email():
-    the_email.delete(0, len(the_email.get()))
-    the_email.insert(0, "")
+    the_email.delete(0, "end")
 
 # Function to show the password entry
 def enter_password():
     global pass_text, the_pass, pass_error
     next.pack_forget()
     the_email.pack_forget()
-    pass_text = tk.Label(account_frame, text = "Password:")
+    pass_text = tk.Label(account_frame, text="Password:")
     pass_text.pack()
-    the_pass = tk.Entry(account_frame, width = 22, show = "*")
+    the_pass = tk.Entry(account_frame, width=22, show="*")
     the_pass.pack()
 
 # Function to delete text of the password
 def empty_pass():
-    the_pass.delete(0, len(the_email.get()))
-    the_pass.insert(0, "")
-    confirm_pass.delete(0, len(the_email.get()))
-    confirm_pass.insert(0, "")
+    the_pass.delete(0, "end")
+    confirm_pass.delete(0, "end")
 
 # ---------- Function to log in ----------
 def signin():
     global next
     enter_email()
-    next = tk.Button(account_frame, text = "Next",
-                     command = verify_email)
+    next = tk.Button(account_frame, text="Next",
+                     command=verify_email)
     next.pack()
 
 # Function to verify if the email exists
@@ -73,17 +70,17 @@ def verify_email():
             user = account[0]
             pin = account[1]
             if user == email:
-                email_error.config(text = email, fg = "#808080")
+                email_error.config(text=email, fg="#808080")
                 enter_password()
-                pass_error = tk.Label(account_frame, text = "")
+                pass_error = tk.Label(account_frame, text="")
                 pass_error.pack()
-                log_in = tk.Button(account_frame, text = "Log in",
-                                   command = verify_password)
+                log_in = tk.Button(account_frame, text="Log in",
+                                   command=verify_password)
                 log_in.pack()
                 break
         if user != email:
-            email_error.config(text = "- Email not founded",
-                               fg = "#FF0000")
+            email_error.config(text="- Email not founded",
+                               fg="#FF0000")
             empty_email()
 
 # Function to verify if the password it's corrects
@@ -93,17 +90,16 @@ def verify_password():
         account_frame.pack_forget()
         menu()
     else:
-        pass_error.config(text = "- Incorrect password",
-                          fg = "#FF0000")
-        the_pass.delete(0, len(the_email.get()))
-        the_pass.insert(0, "")
+        pass_error.config(text="- Incorrect password",
+                          fg="#FF0000")
+        the_pass.delete(0, "end")
 
 # ---------- Function to register ----------
 def signup():
     global next
     enter_email()
-    next = tk.Button(account_frame, text = "Next",
-                     command = create_email)
+    next = tk.Button(account_frame, text="Next",
+                     command=create_email)
     next.pack()
 
 # Function to create an email
@@ -136,16 +132,16 @@ def create_email():
             elif domain not in valid_domains:
                 counter = 3
         if counter == 1:
-            email_error.config(text = "- The email already exists",
-                               fg = "#FF0000")
+            email_error.config(text="- The email already exists",
+                               fg="#FF0000")
             empty_email()
         elif counter == 2:
-            email_error.config(text = "- Invalid username",
-                               fg = "#FF0000")
+            email_error.config(text="- Invalid username",
+                               fg="#FF0000")
             empty_email()
         elif counter == 3:
-            email_error.config(text = "- Invalid domain",
-                               fg = "#FF0000")
+            email_error.config(text="- Invalid domain",
+                               fg="#FF0000")
             empty_email()
         else:
             email_error.config(text = email, fg = "#808080")
@@ -173,22 +169,21 @@ def create_password():
     confirm = confirm_pass.get()
     if password == confirm:
         if password_strength():
-
             hash = hashlib.sha256(password.encode("utf-8"))
             with open('registered_accounts.txt', 'a', encoding="utf-8") as file:
                 file.write(f"{email} {password} {hash.hexdigest()}\n")
             account_frame.pack_forget()
             home()
-            text.config(text = "- Account created succesfully!",
-                        fg = "#008000")
+            text.config(text="- Account created succesfully!",
+                        fg="#008000")
         else:
-            pass_error.config(text = password_requeriments,
-                              font = ("Verdana", 8), fg = "#FF0000",
+            pass_error.config(text=password_requeriments,
+                              font=("Verdana", 8), fg = "#FF0000",
                               justify="left")
             empty_pass()
     else:
-        pass_error.config(text = "- Passwords don't match",
-                          fg = "#FF0000")
+        pass_error.config(text="- Passwords don't match",
+                          fg="#FF0000")
         empty_pass()
 
 # Function to verify the strenght of the password
@@ -208,20 +203,38 @@ def password_strength():
             simbol = True
     return low and up and number and simbol and len(password) >= 10
 
-# Function of the menu
+# Function to the table of plates
+def plate_table():
+    tree = ttk.Treeview(plate)
+    tree["columns"] = ("Name", "Value", "Description", "Available")
+    tree.column("#0", width=0, stretch=tk.NO)
+    tree.column('Name', width=60, anchor=tk.CENTER)
+    tree.column('Value', width=60, anchor=tk.CENTER)
+    tree.column('Description', width=80)
+    tree.column('Available', width=80, anchor=tk.CENTER)
+    tree.heading("#0", text = "", anchor=tk.W)
+    tree.heading('Name', text='Name')
+    tree.heading('Value', text='Value')
+    tree.heading('Description', text='Description')
+    tree.heading('Available', text='Available')
+    tree.pack()
+
+# Function of the top menu
 def menu():
+    global plate
     tab = ttk.Notebook(window)
     
     plate = ttk.Frame(tab)
-    tab.add(plate, text = "Plates")
+    tab.add(plate, text="Plates")
+    plate_table()
     
     table = ttk.Frame(tab)
-    tab.add(table, text = "Tables")
+    tab.add(table, text="Tables")
 
     order = ttk.Frame(tab)
-    tab.add(order, text = "Orders")
+    tab.add(order, text="Orders")
 
-    tab.pack(expand = True, fil = "both")
+    tab.pack(expand=True, fil="both")
 
 # ---------- This is where the code starts running ----------
 if __name__ == '__main__':
@@ -232,8 +245,8 @@ if __name__ == '__main__':
 - a symbol (@*$!?\&/.-_)
 - 10 characters long"""
     window = tk.Tk()
-    icon = tk.PhotoImage(file = "icon.png")
-    logo = tk.PhotoImage(file = "logo.png")
+    icon = tk.PhotoImage(file="icon.png")
+    logo = tk.PhotoImage(file="logo.png")
     window.iconphoto(True, icon)
     window.title("Cafetería: veranera")
     window.geometry("300x300")
