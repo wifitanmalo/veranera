e53ff97046af87f0594b99f002974e5b68449f8c
wifitanmalo <nicolas.chaparro@correounivalle.edu.co>
2023-12-06 00:31:04 -0500

    functions to add, update and delete plates were added

--- veranera.py
+++ veranera.py
@@ -2,7 +2,9 @@ import tkinter as tk
 from tkinter import ttk
 import hashlib
 
-# ---------- Function to the principal page ----------
+# ---------- Principal page ----------
+
+# Function to go to the principal page
 def home():
     global text, home_frame
     home_frame = tk.Frame(window)
@@ -33,7 +35,7 @@ def enter_email():
     email_error.pack()
 
 # Function to delete the text of the email
-def empty_email():
+def clean_email():
     the_email.delete(0, "end")
 
 # Function to show the password entry
@@ -47,11 +49,13 @@ def enter_password():
     the_pass.pack()
 
 # Function to delete text of the password
-def empty_pass():
+def clean_pass():
     the_pass.delete(0, "end")
     confirm_pass.delete(0, "end")
 
-# ---------- Function to log in ----------
+# ---------- Log in ----------
+
+# Function to log in
 def signin():
     global next
     enter_email()
@@ -62,7 +66,7 @@ def signin():
 # Function to verify if the email exists
 def verify_email():
     global pin, pass_error
-    email = the_email.get()
+    email = the_email.get().lower()
     with open('registered_accounts.txt', 'r') as email_file:
         accounts_list = email_file.readlines()
         for line in range(0, len(accounts_list)):
@@ -81,7 +85,7 @@ def verify_email():
         if user != email:
             email_error.config(text="- Email not founded",
                                fg="#FF0000")
-            empty_email()
+            clean_email()
 
 # Function to verify if the password it's corrects
 def verify_password():
@@ -94,7 +98,9 @@ def verify_password():
                           fg="#FF0000")
         the_pass.delete(0, "end")
 
-# ---------- Function to register ----------
+# ---------- Sign up ----------
+
+# Function to register an user
 def signup():
     global next
     enter_email()
@@ -110,7 +116,7 @@ def create_email():
                     "@outlook.com", "@correounivalle.edu.co"]
     counter = 0
     try:
-        email = the_email.get()
+        email = the_email.get().lower()
         username = email[0:email.index("@")]
         domain = email[email.index("@"):len(email)]
         if len(username) == 0:
@@ -134,15 +140,15 @@ def create_email():
         if counter == 1:
             email_error.config(text="- The email already exists",
                                fg="#FF0000")
-            empty_email()
+            clean_email()
         elif counter == 2:
             email_error.config(text="- Invalid username",
                                fg="#FF0000")
-            empty_email()
+            clean_email()
         elif counter == 3:
             email_error.config(text="- Invalid domain",
                                fg="#FF0000")
-            empty_email()
+            clean_email()
         else:
             email_error.config(text = email, fg = "#808080")
             enter_password()
@@ -158,7 +164,7 @@ def create_email():
                                  command = create_password)
             register.pack()
     except:
-        empty_email()
+        clean_email()
         email_error.config(text = "- Error, try again",
                            fg = "#FF0000")
 
@@ -180,11 +186,11 @@ def create_password():
             pass_error.config(text=password_requeriments,
                               font=("Verdana", 8), fg = "#FF0000",
                               justify="left")
-            empty_pass()
+            clean_pass()
     else:
         pass_error.config(text="- Passwords don't match",
                           fg="#FF0000")
-        empty_pass()
+        clean_pass()
 
 # Function to verify the strenght of the password
 def password_strength():
@@ -203,36 +209,158 @@ def password_strength():
             simbol = True
     return low and up and number and simbol and len(password) >= 10
 
-# Function to the table of plates
-def plate_table():
-    tree = ttk.Treeview(plate)
-    tree["columns"] = ("Name", "Value", "Description", "Available")
+# ---------- Log out ----------
+def logout():
+    home()
+    tab.pack_forget()
+
+# ---------- Plates ----------
+
+# Function to clean the plate's entrys
+def clean_plate_entrys():
+    name_entry.delete(0, "end")
+    value_entry.delete(0, "end")
+    description_entry.delete(0, "end")
+    available_entry.delete(0, "end")
+
+# Function to get the values of the plate's entrys
+def plate_data():
+    global name, value, description, available
+    name = name_entry.get()
+    value = float(value_entry.get())
+    description = description_entry.get()
+    available = available_entry.get().capitalize()
+
+# Function to add a new plate in the table
+def add_plate():
+    available_list = ["Yes", "No"]
+    try:
+        plate_data()
+        if len(name) == 0 or len(description) == 0:
+            add_error.config(text="- There cannot be empty spaces",
+                            fg="#FF0000")
+            clean_plate_entrys()
+        elif available not in available_list:
+            add_error.config(text="- Available only gets (Yes/No)",
+                            fg="#FF0000")
+            clean_plate_entrys()
+        else:
+                # Agregar el elemento al Treeview
+            plates.insert('', 'end', values=(name, value, description, available))
+            clean_plate_entrys()
+            add_error.config(text="")
+    except:
+        add_error.config(text="- Value only gets positive numbers",
+                            fg="#FF0000")
+        clean_plate_entrys()
+
+# Function to update the dates of a plate
+def update_plate():
+    seleccion = plates.selection()
+    if seleccion:
+        plate_data()
+        if name and value:
+            plates.item(seleccion, values=(name, value, description, available))
+            clean_plate_entrys()
+
+# Function to delete a plate
+def delete_plate():
+    seleccion = plates.selection()
+    print(plates.item(seleccion, "values")[0])
+    if seleccion:
+        plates.delete(seleccion)
+
+# Function to generate the plate's table
+def plates_table():
+    global plates, name_entry, value_entry, description_entry, available_entry, add_error
+    columns = ("Name", "Value", "Description", "Available")
+    plates = ttk.Treeview(plate, columns=columns, show="headings")
+    plates.column("#0", width=0, stretch=tk.NO)
+    plates.column("Name", width=60, anchor=tk.CENTER)
+    plates.column("Value", width=60, anchor=tk.CENTER)
+    plates.column("Description", width=80)
+    plates.column("Available", width=80, anchor=tk.CENTER)
+    plates.heading("#0", text = "", anchor=tk.W)
+    plates.heading("Name", text="Name")
+    plates.heading("Value", text="Value")
+    plates.heading("Description", text="Description")
+    plates.heading("Available", text="Available")
+    plates.pack()
+    add_frame = tk.Frame(plate)
+    add_frame.pack()
+    name_text = tk.Label(add_frame, text="Name:")
+    name_text.pack()
+    name_entry = tk.Entry(add_frame)
+    name_entry.pack()
+    value_text = tk.Label(add_frame, text="Value:")
+    value_text.pack()
+    value_entry = tk.Entry(add_frame)
+    value_entry.pack()
+    description_box = tk.Label(add_frame, text="Description:")
+    description_box.pack()
+    description_entry = tk.Entry(add_frame)
+    description_entry.pack()
+    available_box = tk.Label(add_frame, text="Available:")
+    available_box.pack()
+    available_entry = tk.Entry(add_frame)
+    available_entry.pack()
+    add_error = tk.Label(add_frame, text="")
+    add_error.pack()
+    add = tk.Button(add_frame, text="Add", command=add_plate)
+    add.pack()
+    update = tk.Button(add_frame, text="Update", command=update_plate)
+    update.pack()
+    delete = tk.Button(add_frame, text="Delete", command=delete_plate)
+    delete.pack()
+
+# ---------- Tables ----------
+def tables():
+    tree = ttk.Treeview(table)
+    tree["columns"] = ("Table", "Date", "Time", "People amount")
     tree.column("#0", width=0, stretch=tk.NO)
-    tree.column('Name', width=60, anchor=tk.CENTER)
-    tree.column('Value', width=60, anchor=tk.CENTER)
-    tree.column('Description', width=80)
-    tree.column('Available', width=80, anchor=tk.CENTER)
+    tree.column('Table', width=60, anchor=tk.CENTER)
+    tree.column("Date", width=60, anchor=tk.CENTER)
+    tree.column("Time", width=60)
+    tree.column("People amount", width=100, anchor=tk.CENTER)
     tree.heading("#0", text = "", anchor=tk.W)
-    tree.heading('Name', text='Name')
-    tree.heading('Value', text='Value')
-    tree.heading('Description', text='Description')
-    tree.heading('Available', text='Available')
+    tree.heading('Table', text='Table')
+    tree.heading("Date", text="Date")
+    tree.heading("Time", text="Time")
+    tree.heading("People amount", text="People amount")
+    tree.pack()
+
+# ---------- Orders ----------
+def orders():
+    tree = ttk.Treeview(order)
+    tree["columns"] = ("Table's name", "Table's number")
+    tree.column("#0", width=0, stretch=tk.NO)
+    tree.column("Table's name", width=140, anchor=tk.CENTER)
+    tree.column("Table's number", width=140, anchor=tk.CENTER)
+    tree.heading("#0", text = "", anchor=tk.W)
+    tree.heading("Table's name", text="Table's name")
+    tree.heading("Table's number", text="Table's number")
     tree.pack()
 
 # Function of the top menu
 def menu():
-    global plate
+    global tab, plate, table, order
     tab = ttk.Notebook(window)
     
     plate = ttk.Frame(tab)
     tab.add(plate, text="Plates")
-    plate_table()
+    plates_table()
     
     table = ttk.Frame(tab)
     tab.add(table, text="Tables")
+    tables()
 
     order = ttk.Frame(tab)
     tab.add(order, text="Orders")
+    orders()
+
+    log_out = ttk.Frame(tab)
+    logout_button = tk.Button(tab, text="Log out", command=logout)
+    logout_button.pack()
 
     tab.pack(expand=True, fil="both")
 
@@ -249,7 +377,7 @@ if __name__ == '__main__':
     logo = tk.PhotoImage(file="logo.png")
     window.iconphoto(True, icon)
     window.title("Cafetería: veranera")
-    window.geometry("300x300")
+    window.geometry("300x510")
     window.resizable(False, False)
     home()
     window.mainloop()
